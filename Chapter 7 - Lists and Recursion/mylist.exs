3b9eb2353ff7abc48777f71a988eead6cc4c0a44
Oktaviandi Hadi Nugraha <oktaviandi@gmail.com>
2023-04-21 15:05:49 +0700

    Add weather history

--- Chapter 7 - Lists and Recursion/mylist.exs
+++ Chapter 7 - Lists and Recursion/mylist.exs
@@ -37,4 +37,8 @@ defmodule MyList do
       new_char -> new_char
     end
   end
+
+  def swap([]), do: []
+  def swap([_]), do: raise "Can't swap a list with an odd number of elements"
+  def swap([a, b | tail]), do: [b, a | swap tail]
 end
---
+++ Chapter 7 - Lists and Recursion/weather_history.exs
@@ -0,0 +1,23 @@
+defmodule WeatherHistory do
+  def for_location_27([]), do: []
+
+  def for_location_27([[timestamp, 27, temperature, rainfall] | tail]),
+    do: [[timestamp, 27, temperature, rainfall] | for_location_27(tail)]
+
+  def for_location_27([_ | tail]), do: for_location_27(tail)
+
+  def test_data do
+    [
+      [1_366_225_622, 26, 15, 0.125],
+      [1_366_225_622, 27, 15, 0.45],
+      [1_366_225_622, 28, 21, 0.25],
+      [1_366_229_222, 26, 19, 0.081],
+      [1_366_229_222, 27, 17, 0.468],
+      [1_366_229_222, 28, 15, 0.60],
+      [1_366_232_822, 26, 22, 0.095],
+      [1_366_232_822, 27, 21, 0.05],
+      [1_366_232_822, 28, 24, 0.03],
+      [1_366_236_422, 26, 17, 0.025]
+    ]
+  end
+end
