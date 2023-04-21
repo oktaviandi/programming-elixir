b81bd75cfc8efe42b25f917b8fdfccbf5649aeab
Oktaviandi Hadi Nugraha <oktaviandi@gmail.com>
2023-04-21 14:43:57 +0700

    Add caesar function

--- Chapter 7 - Lists and Recursion/mylist.exs
+++ Chapter 7 - Lists and Recursion/mylist.exs
@@ -1,11 +1,14 @@
 defmodule MyList do
+  def len([]), do: 0
+  def len([_ | tail]), do: 1 + len(tail)
+
   def square([]), do: []
   def square([head | tail]), do: [ head * head | square(tail)]
 
   def add_1([]), do: []
   def add_1([head | tail]), do: [head + 1 | add_1(tail)]
 
-  def map([], _func), do: []
+  def map([], _), do: []
   def map([head | tail], func), do: [func.(head) | map(tail, func)]
 
   def reduce([], initial, _func), do: initial
@@ -16,4 +19,22 @@ defmodule MyList do
   def map_sum(list), do: sum(map(list, &(&1 * &1)))
 
   def max(list), do: reduce(list, 0, &(max(&1, &2)))
-end 
+
+  def caesar(list, n) when is_list(list) and is_integer(n) do
+    map(list, fn char -> char_overflow(char, n) end)
+  end
+
+  defp char_overflow(char, n) when char > 64 and char < 91 do
+    case char + n do
+      new_char when new_char > 90 -> new_char - 26
+      new_char -> new_char
+    end
+  end
+
+  defp char_overflow(char, n) when char > 96 and char < 123 do
+    case char + n do
+      new_char when new_char > 122 -> new_char - 26
+      new_char -> new_char
+    end
+  end
+end
